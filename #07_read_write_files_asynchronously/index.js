76b7e18a935ae9802ba53ebc550fa3da0bb7a1cd
NayemRahman62 <rahmannayem62@gmail.com>
2024-10-09 10:37:23 +0600

    Accessing input.txt through data1

--- #07_read_write_files_asynchronously/index.js
+++ #07_read_write_files_asynchronously/index.js
@@ -4,6 +4,9 @@ const fs=require('node:fs');
 
 fs.readFile('./Files/start.txt','utf-8', (error1, data1)=>{
   console.log(data1)
+  fs.readFile(`./Files/${data1}.txt`,'utf-8', (error2,data2)=>{
+    console.log(data2)
+  })
 })
 
 console.log('Reading File...')
