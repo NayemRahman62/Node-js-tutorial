694b7511feb43465f606959654bda29d8a05e8e3
NayemRahman62 <rahmannayem62@gmail.com>
2024-10-09 10:49:47 +0600

    Writing in output.txt

--- #07_read_write_files_asynchronously/index.js
+++ #07_read_write_files_asynchronously/index.js
@@ -6,6 +6,12 @@ fs.readFile('./Files/start.txt','utf-8', (error1, data1)=>{
   console.log(data1)
   fs.readFile(`./Files/${data1}.txt`,'utf-8', (error2,data2)=>{
     console.log(data2)
+    fs.readFile('./Files/append.txt','utf-8',(error3,data3)=>{
+      console.log(data3)
+      fs.writeFile('./Files/output.txt', `${data1}\n\n${data2}\n\n${data3}\n\nDate created:${new Date()}`, ()=>{
+        console.log('File written successfully')
+      })
+    })
   })
 })
 
